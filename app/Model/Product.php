073faff2eebf5acb6576c696507fa4e4d6429f3a
Khaled Elfishawy <khalidelfishawy@gmail.com>
2022-01-17 14:34:57 +0200

    Update Product.php

--- app/Model/Product.php
+++ app/Model/Product.php
@@ -25,7 +25,7 @@ class Product extends Model
 
     public function scopeActive($query)
     {
-        return true;
+        return $query->whereNotNull('status');
     }
 
     public function reviews()
